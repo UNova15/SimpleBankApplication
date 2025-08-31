42ca08f20720f7ecd370fc9d1a4a855b9cfa0e73
Kirill <kkrecetov5@gmail.com>
2025-08-31 16:59:04 +0700

    Setting up a project

---
+++ src/main/java/org/simplebank/Application.java
@@ -0,0 +1,11 @@
+package org.simplebank;
+
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+    public static void main(String [] args){
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+
+    }
+}
